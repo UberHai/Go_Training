03660dc2cbb25fd98c596e82ba1a9c3b1407b93c
uber.hai.team <uber.hai.team@gmail.com>
2017-10-03 20:29:29 -0400

    Shorthand Variables

---
+++ 04_variables/01_shorthand/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+  
+  a := 10
+  b := "Golang"
+  c := 4.17
+  d:= true
+  
+  fmt.Printf("%v \t %T \n", a, a)
+  fmt.Printf("%v \t %T \n", b, b)
+  fmt.Printf("%v \t %T \n", c, c)
+  fmt.Printf("%v \t %T \n", d, d)
+}
