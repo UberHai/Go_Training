c6fda58e08cc551da6192825d916ef2662f78b13
uber.hai.team <uber.hai.team@gmail.com>
2017-10-09 00:32:58 -0400

    fib and for loop

---
+++ 10_fib-and-for-loop/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+// Fib returns the nth number in the Fibonacci series.
+func Fib(n int) int {
+	if n < 2 {
+		return n
+	}
+	return Fib(n-1) + Fib(n-2)
+}
+
+func main() {
+	//Print the 8th number of Fibonacci series
+	fmt.Println(Fib(8))
+
+	//Print outer and inner loop sequence
+	for i := 0; i < 5; i++ {
+		for j := 0; j < 5; j++ {
+			fmt.Println(i, " - ", j)
+		}
+	}
+}
